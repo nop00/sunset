60268a191ff8cfe71ed06348eb83c87d61cab578
Grégoire Dubourg <gregoire.dubourg@gmail.com>
2021-01-24 23:25:28 +0100

    Curseurs verticaux

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Ephemeris from "./components/ephemeris";
 import data from "../data/ephemeris.json";
 import HorizontalCursor from "./components/horizontal-cursor";
 import "./styles.css";
 import { x } from "@xstyled/styled-components";
 import { map, split, join } from "lodash";
+import VerticalRangeSlider from "./components/vertical-range-slider";
 
 const SUMMER = new Date("2021-03-28");
 const WINTER = new Date("2021-10-31");
@@ -28,8 +29,25 @@ export default () => {
     return day;
   });
 
+  const [lightsOnTime, setLightsOnTime] = useState(0);
+  const [lightsOffTime, setLightsOffTime] = useState(24 * 60);
+
   return (
     <>
+      <VerticalRangeSlider
+        from={0}
+        to={10 * 60}
+        value={lightsOnTime}
+        onChange={setLightsOnTime}
+      />
+      <VerticalRangeSlider
+        from={16 * 60}
+        to={24 * 60}
+        value={lightsOffTime}
+        onChange={setLightsOffTime}
+      />
+      <div>Allumage : {lightsOnTime}</div>
+      <div>Extinction : {lightsOffTime}</div>
       <x.div
         w={800}
         h={500}
@@ -39,7 +57,6 @@ export default () => {
       >
         <Ephemeris data={dataWithSavings} />
       </x.div>
-      {/*<HorizontalCursor data={data} />*/}
     </>
   );
 };
---
+++ src/components/vertical-range-slider.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface Props {
+  from: number;
+  to: number;
+  value: number;
+  onChange: (event: any) => void;
+}
+
+export default ({ from, to, value, onChange }: Props) => {
+  const onSlide = (e) => {onChange(e.target.value)};
+
+  return (
+    <div className="slider-wrapper">
+      <input
+        type="range"
+        min={from}
+        max={to}
+        defaultValue={value}
+        step="1"
+        onChange={onSlide}
+      />
+    </div>
+  );
+};
